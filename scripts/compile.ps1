c98ed7532c742415b7e450b507b71206a415ee2f
André Vennberg <andre.vennberg@gmail.com>
2019-01-21 00:30:58 +0100

    Removed old scripts and configs.

--- scripts/compile.ps1
+++
@@ -1,9 +0,0 @@
-$file = $args[0]
-
-$basename = (Get-Item $file).BaseName
-
-aqsl -o shaders/$basename.slx $file 2>&1 | ForEach-Object {
-    if (($_ | Out-String).Contains($file)) {
-        Write-Host $_
-    }
-}
--- scripts/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIR=$(dirname "$1")
-FILE=$(basename "$1" .sl)
-
-aqsl -o build/$FILE.slx $1
